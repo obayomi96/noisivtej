723c6cf64269960b5aab13378ee875a12a6c6aab
obayomi96 <obayomimartins96@gmail.com>
2023-12-13 08:18:10 +0100

    fix build

--- src/components/LogisticsSummary.tsx
+++ src/components/LogisticsSummary.tsx
@@ -1,30 +1,28 @@
-import React, { useState } from "react";
-import { createRoot } from "react-dom/client";
 import { AgChartsReact } from "ag-charts-react";
-import { AgChartOptions, AgCharts } from "ag-charts-community";
+import { AgChartOptions } from "ag-charts-community";
+
+const charOpt: AgChartOptions = {
+  data: [
+    { asset: "China", amount: 60000 },
+    { asset: "Cocoa exports", amount: 40000 },
+    { asset: "West Africa", amount: 7000 },
+    { asset: "Ghana", amount: 5000 },
+    { asset: "Europe ", amount: 30000 },
+  ],
+  title: {
+    text: "Quick overview of your logistics",
+  },
+  series: [
+    {
+      type: "pie",
+      angleKey: "amount",
+      legendItemKey: "asset",
+    },
+  ],
+};
 
 const LogisticsChart = () => {
-  const [options, setOptions] = useState<AgChartOptions>({
-    data: [
-      { asset: "China", amount: 60000 },
-      { asset: "Cocoa exports", amount: 40000 },
-      { asset: "West Africa", amount: 7000 },
-      { asset: "Ghana", amount: 5000 },
-      { asset: "Europe ", amount: 30000 },
-    ],
-    title: {
-      text: "Quick overview of your logistics",
-    },
-    series: [
-      {
-        type: "pie",
-        angleKey: "amount",
-        legendItemKey: "asset",
-      },
-    ],
-  });
-
-  return <AgChartsReact options={options} />;
+  return <AgChartsReact options={charOpt} />;
 };
 
 export default LogisticsChart;
